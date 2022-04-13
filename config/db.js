dde12446d787c7a778de1d492614fff153869f38
Ahmed Bougatf <98043267+bougatfa8@users.noreply.github.com>
2022-04-13 14:07:09 +0200

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+"mongodb+srv://mern:mern@cluster0.wu5bo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Failed to connect to MongoDB", err));
